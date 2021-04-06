d392239de8356f6318d30c15e0a5b0c818ece48e
Itamar Silva <itamar_minnow@hotmail.com>
2021-04-05 22:37:24 -0300

    Validando CPF existente

--- 01-Trilha-NodeJS/02-FinAPI/src/index.js
+++ 01-Trilha-NodeJS/02-FinAPI/src/index.js
@@ -10,13 +10,19 @@ const customers = [];
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
-  const id = uuidv4();
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
 
   customers.push({
     cpf,
     name,
-    id,
-    statement: []
+    id: uuidv4(),
+    statement: [],
   });
 
   return response.status(201).send();
