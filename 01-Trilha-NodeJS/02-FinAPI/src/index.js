d5d010d4b6b3bffde2429d4c2928fddc54d27eca
Itamar Silva <itamar_minnow@hotmail.com>
2021-07-12 23:20:02 -0300

    Criando saque na conta

--- 01-Trilha-NodeJS/02-FinAPI/src/index.js
+++ 01-Trilha-NodeJS/02-FinAPI/src/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -20,6 +21,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   request.customer = customer;
 
   return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
 }
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
@@ -54,12 +67,33 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   const { description, amount } = request.body;
 
   const { customer } = request;
-  
+
   const statementOperation = {
     description,
     amount,
     created_at: new Date(),
     type: "credit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw",verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({error: "Insufficient funds"});
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
   };
 
   customer.statement.push(statementOperation);
