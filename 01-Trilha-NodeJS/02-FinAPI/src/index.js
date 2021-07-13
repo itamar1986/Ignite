f82931424d54b1b235c779c0f429cda693f42499
Itamar Silva <itamar_minnow@hotmail.com>
2021-07-12 21:26:54 -0300

    Listando extrato

--- 01-Trilha-NodeJS/02-FinAPI/src/index.js
+++ 01-Trilha-NodeJS/02-FinAPI/src/index.js
@@ -20,12 +20,20 @@ app.post("/account", (request, response) => {
 
   customers.push({
     cpf,
-    name,
+    name, 
     id: uuidv4(),
     statement: [],
   });
 
   return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
