4fbdbb7926656e087f3fa28a0acf8d3a1051884e
Itamar Silva <itamar_minnow@hotmail.com>
2021-07-13 22:00:57 -0300

    Remover conta

--- 01-Trilha-NodeJS/02-FinAPI/src/index.js
+++ 01-Trilha-NodeJS/02-FinAPI/src/index.js
@@ -45,8 +45,6 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-//app.use(verifyIfExistsAccountCPF);
-
 app.get("/statement", verifyIfExistsAccountCPF,(request, response) => {
   const { customer } = request;
   return response.json(customer.statement);
@@ -84,9 +82,7 @@ app.post("/withdraw",verifyIfExistsAccountCPF, (request, response) => {
 app.get("/statement/date", verifyIfExistsAccountCPF,(request, response) => {
   const { customer } = request;
   const { date } = request.query;
-
   const dateFormat = new Date(date + " 00:00");
-
   const statement = customer.statement.filter(
     (statement) => 
     statement.created_at.toDateString() === 
@@ -98,16 +94,25 @@ app.get("/statement/date", verifyIfExistsAccountCPF,(request, response) => {
 app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
   const { name } = request.body;
   const { customer } = request;
-
   customer.name = name;
-
   return response.status(201).send();
 });
 
 app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
-
   return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  customers.splice(customer, 1);
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const balance = getBalance(customer.statement);
+  return response.json(balance);
 });
 
 app.listen(3333);
