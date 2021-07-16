0bd3d32b5c5b49eec70d8705106456a1d9add58d
Itamar Silva <itamar_minnow@hotmail.com>
2021-07-16 20:52:10 -0300

    Conhecendo Singleton Pattern

--- 01-Trilha-NodeJS/04-RentalX/src/modules/cars/useCases/listCategories/index.ts
+++ 01-Trilha-NodeJS/04-RentalX/src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./listCategoriesUseCase";
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getIntance();
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 const listCategoriesController = new ListCategoriesController(
   listCategoriesUseCase
