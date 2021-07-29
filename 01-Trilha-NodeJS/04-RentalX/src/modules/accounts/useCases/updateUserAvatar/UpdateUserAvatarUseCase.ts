e61d3ec8e4295a2ebf3aeaf09cb695b21d384b68
Itamar Silva <itamar_minnow@hotmail.com>
2021-07-28 21:54:02 -0300

    Adicionando coluna de avatar

---
+++ 01-Trilha-NodeJS/04-RentalX/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,10 @@
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+class UpdateUserAvatarUseCase {
+  async execute({ user_id, avatar_file }: IRequest) {}
+}
+
+export { UpdateUserAvatarUseCase };
