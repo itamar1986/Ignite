08f243a7bad61ae12c5c60d4c91d362b4e005139
Itamar Silva <itamar_minnow@hotmail.com>
2021-08-09 22:16:33 -0300

    TDD na prática

--- 01-Trilha-NodeJS/04-RentalX/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ 01-Trilha-NodeJS/04-RentalX/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -1,10 +1,32 @@
 import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 
 import { ICarsRepository } from "../ICarsRepository";
 
 class CarsRepositoryInMemory implements ICarsRepository {
-  create(data: ICreateCarDTO): Promise<void> {
-    throw new Error("Method not implemented.");
+  cars: Car[] = [];
+
+  async create({
+    brand,
+    category_id,
+    daily_rate,
+    description,
+    fine_amount,
+    name,
+    license_plate,
+  }: ICreateCarDTO): Promise<void> {
+    const car = new Car();
+
+    Object.assign(car, {
+      brand,
+      category_id,
+      daily_rate,
+      description,
+      fine_amount,
+      name,
+      license_plate,
+    });
+    this.cars.push(car);
   }
 }
 
--- 01-Trilha-NodeJS/04-RentalX/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ 01-Trilha-NodeJS/04-RentalX/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,6 +1,6 @@
 import { CategoriesRepositoryInMemory } from "@modules/cars/repositories/in-memory/CategoriesRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
-import { AppError } from "../../../../errors/AppError";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 let createCategoryUseCase: CreateCategoryUseCase;
