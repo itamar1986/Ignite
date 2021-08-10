f6c7facb09fc29c4cd138302827207bc3817ecd7
Itamar Silva <itamar_minnow@hotmail.com>
2021-08-09 21:11:06 -0300

    TDD na prática

---
+++ 01-Trilha-NodeJS/04-RentalX/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,11 @@
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    createCarUseCase = new CreateCarUseCase();
+  });
+
+  it("should be able to create a new car", () => {});
+});
