42c2034a67b458579506d31fe09fc61f7322259e
Itamar Silva <itamar_minnow@hotmail.com>
2021-08-12 18:26:24 -0300

    Continuando o caso de uso de carros

--- 01-Trilha-NodeJS/04-RentalX/src/modules/cars/dtos/ICreateCarDTO.ts
+++ 01-Trilha-NodeJS/04-RentalX/src/modules/cars/dtos/ICreateCarDTO.ts
@@ -1,11 +1,3 @@
-interface ICreateCarDTO {
-  name: string;
-  description: string;
-  daily_rate: number;
-  license_plate: string;
-  fine_amount: number;
-  brand: string;
-  category_id: string;
-}
+interface ICreateCarDTO {}
 
 export { ICreateCarDTO };
