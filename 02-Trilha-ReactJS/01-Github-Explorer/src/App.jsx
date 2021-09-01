2f5a8afc77b9c8cf868358d5f086d8f4ac013422
Itamar Santos da Silva <itamar_minnow@hotmail.com>
2021-09-01 20:59:40 -0300

    Primeiro componente React

--- 02-Trilha-ReactJS/01-Github-Explorer/src/App.jsx
+++ 02-Trilha-ReactJS/01-Github-Explorer/src/App.jsx
@@ -1,7 +1,8 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-  //throw new Error('Configuração do Ignite modulo ReactJS');
-
-  return <h1>Hello World</h1>
+  /* throw new Error('Configuração do Ignite modulo ReactJS');
+  return <h1>Hello World</h1> */
+  return <RepositoryList />
 }
