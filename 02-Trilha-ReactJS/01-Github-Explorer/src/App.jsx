521cdfa9aaf6e166e881cc9077faf226e774bd31
Itamar Santos da Silva <itamar_minnow@hotmail.com>
2021-09-02 20:24:30 -0300

    Estado do componente

--- 02-Trilha-ReactJS/01-Github-Explorer/src/App.jsx
+++ 02-Trilha-ReactJS/01-Github-Explorer/src/App.jsx
@@ -1,8 +1,14 @@
+import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
   /* throw new Error('Configuração do Ignite modulo ReactJS');
   return <h1>Hello World</h1> */
-  return <RepositoryList />
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  )
 }
